9954c97b01dcbfa4ace1b2f8c01fe2792024c2ef
Roshan <nahsorad@gmail.com>
2020-05-30 13:24:48 +0545

    Rename chat.js => script.js

--- public/chat.js
+++
@@ -1,5 +0,0 @@
-console.log('Hello World!');
-// Make WebSocket connection
-// We have access to 'io' since we have linked the socket.io library
-let socket = io.connect('http://localhost:8080');
-io.disconnect();
