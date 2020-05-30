3e0ae997e493cb3fb16fea09c89537c3bbb95e7c
Roshan <nahsorad@gmail.com>
2020-05-30 14:56:22 +0545

    Change innerHTML to show stdout

--- public/script.js
+++ public/script.js
@@ -3,19 +3,16 @@ console.log('Hello World!');
 // We have access to 'io' since we have linked the socket.io library
 let socket = io.connect('http://localhost:8080');
 
-let p = document.getElementById('messageElement');
-let btn = document.getElementById('btn');
+let p = document.getElementById('log');
+let buildBtn = document.getElementById('btn');
 
 btn.addEventListener('click', () => {
-    socket.emit('clientEmit', {
-        message: 'Hello Server!'
+    socket.emit('buildImage', {
+        message: 'Build the Dockerfile!'
     });
-});
-
-socket.on('serverEmit', data => {
-    p.innerHTML = `Message from server: ${data.message}`;
 });
 
 socket.on('stdout', data => {
     console.dir(data);
+    p.innerHTML += data + "<br />";
 })
