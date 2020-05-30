88da9f9b378797b779d2144e2d15682b337e5144
Roshan <nahsorad@gmail.com>
2020-05-30 12:16:05 +0545

    Setup socket.io on the server

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const socket = require('socket.io');
+
+const app = express();
+
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const server = app.listen(8080, () => {
+    console.log('Server is now listening on port 8080');
+});
+
+const io = socket(server);
+
+// Need to setup socket.io on the client (frontend) as well ...
+// ... for the 'connection' event to fire
+io.on('connection', socket => {
+    console.log(`New socket connection: ${socket.id}`);
+});
