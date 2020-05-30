34eac458b2f7a403b83993b476a2ba0a573a16fa
Roshan <nahsorad@gmail.com>
2020-05-30 13:25:08 +0545

    Listen for serverEmit and emit client data

---
+++ public/script.js
@@ -0,0 +1,17 @@
+console.log('Hello World!');
+// Make WebSocket connection
+// We have access to 'io' since we have linked the socket.io library
+let socket = io.connect('http://localhost:8080');
+
+let p = document.getElementById('messageElement');
+let btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+    socket.emit('clientEmit', {
+        message: 'Hello Server!'
+    });
+});
+
+socket.on('serverEmit', data => {
+    p.innerHTML = `Message from server: ${data.message}`;
+});
